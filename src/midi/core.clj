c9831635437eda30f497f8264a17bba28c60a2af
Andy Marks <vampwillow@gmail.com>
2017-08-18 19:58:32 +1000

    Extracted sysex constants for better readability

--- src/midi/core.clj
+++ src/midi/core.clj
@@ -1,14 +1,16 @@
 (ns midi.core
   (:import [javax.sound.midi MidiSystem Receiver ShortMessage SysexMessage]))
 
+(def ^:const SYSEX_HEADER [240 0 32 41 2 24])
+(def ^:const SYSEX_FOOTER [247])
+
 (defn send-midi [{:keys [out]} & args]
   (.send out
          (doto (ShortMessage.) (.setMessage (nth args 0) (nth args 1) (nth args 2)))
          -1))
 
 (defn send-midi-sysex [{:keys [out]} & args]
-  (let [contents (byte-array (concat [240 0 32 41 2 24] args [247]))
-        _ (println contents)]
+  (let [contents (byte-array (concat SYSEX_HEADER args SYSEX_FOOTER))]
     (.send out
            (doto (SysexMessage.) (.setMessage contents (count contents)))
            -1)))
