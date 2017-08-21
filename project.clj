e58d470e9acf5037cd5b3b7db9667552fd31e9a8
Andy Marks <vampwillow@gmail.com>
2017-08-21 20:08:01 +1000

    Version bump for new demo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-launchpad-mk2 "0.1.0"
+(defproject clj-launchpad-mk2 "0.1.1"
   :description "Novation Launchpad MK2 library for clojure"
   :url "https://github.com/andeemarks/clj-launchpad-mk2"
   :license {:name "MIT"
