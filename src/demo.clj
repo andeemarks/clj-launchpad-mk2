b6fcbfb1ba2044a41cc72e59d92b78ccf4e7b32f
Andy Marks <vampwillow@gmail.com>
2017-08-21 20:07:31 +1000

    DRYed up demo - still need to better way to handle key presses

--- src/demo.clj
+++ src/demo.clj
@@ -1,18 +1,18 @@
 (ns demo
   (:require [clj-launchpad-mk2 :refer :all]))
 
-(defn wipe-left-to-right [lpad]
+(defn- wipe-left-to-right [lpad]
   (doseq [x (range 0 8)]
   (light-column lpad x 66)
   (Thread/sleep 200)
   (light-column lpad x 0)))
 
-(defn wipe-top-to-bottom [lpad]
+(defn- wipe-top-to-bottom [lpad]
   (doseq [y (range 0 8)]
   (light-row lpad y 66)
   (Thread/sleep 200)))
 
-(defn flash-border [lpad]
+(defn- flash-border [lpad]
   (doseq [y (range 0 8)
           x [0 7]]
     (flash lpad x y 60))
@@ -20,52 +20,46 @@
           y [0 7]]
     (flash lpad x y 60)))
 
-(defn pulse-middle [lpad]
+(defn- pulse-middle [lpad]
   (doseq [x (range 1 7)
           y (range 1 7)]
-    (pulse lpad x y 60)))
+    (pulse lpad x y 60))
+  (Thread/sleep 1000))
 
-(defn brightness-quadrants [lpad]
+(defn- brightness-quadrants [lpad]
   (doseq [x (range 0 4)
-          y (range 0 4)]
-    (rgb lpad x y (rand-int 64) 0 0))
-  (doseq [x (range 0 4)
-          y (range 4 8)]
-    (rgb lpad x y  0 (rand-int 64) 0))
-  (doseq [x (range 4 8)
-          y (range 4 8)]
-    (rgb lpad x y  0 0 (rand-int 64)))
-  (doseq [x (range 4 8)
           y (range 0 4)
-          :let [brightness (rand-int 64)]]
-    (rgb lpad x y  brightness brightness brightness)))
+          :let [brightness (* 4 (+ x (* 4 y)))]]
+    (rgb lpad x y brightness 0 0) ; bottom-left => red
+    (rgb lpad (+ x 4) y  0 brightness 0) ; bottom-right => green
+    (rgb lpad x (+ y 4)  0 0 brightness) ; top-left => blue
+    (rgb lpad (+ x 4) (+ y 4) brightness brightness brightness) ; top-right => white/gray
+    )
+  (Thread/sleep 2000))
 
-(defn light-pressed-button [lpad]
+(defn- handle-button-press [lpad]
+  (fn [msg timestamp]
+    (let [x (:x msg)
+          y (:y msg)
+          color (- 127 (+ x y))]
+      (light-cell lpad x y color))))
+
+(defn- light-pressed-button [lpad]
   (scroll-text-once lpad "Touch me!" 54)
 
-  (set-button-press-handler     
-    lpad    
-    (fn 
-      [msg timestamp]
-      (light-cell 
-        lpad 
-        (:x msg) 
-        (:y msg) 
-        (- 127 (+ (:x msg) (:y msg))))))  )
+  (set-button-press-handler lpad (handle-button-press lpad))
+  (Thread/sleep 5000))
 
 (defn -main [& args]
   (let [lpad (open)]
-    (light-grid lpad 23)      
-    (clear-grid lpad)       
-    (wipe-left-to-right lpad)
-    (wipe-top-to-bottom lpad)
-    (flash-border lpad)
-    (pulse-middle lpad)
-    (Thread/sleep 1000)
-    (clear-grid lpad)       
-    (brightness-quadrants lpad)
-    (Thread/sleep 2000)
-    (clear-grid lpad)       
-    (light-pressed-button lpad)
-    (close lpad)
-  ))
+    (doto lpad
+      (clear-grid)       
+      (wipe-left-to-right)
+      (wipe-top-to-bottom)
+      (flash-border)
+      (pulse-middle)
+      (clear-grid)       
+      (brightness-quadrants)
+      (clear-grid)       
+      (light-pressed-button)
+      (close))))
